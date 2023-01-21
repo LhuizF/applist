29cfe6515254adb74e55d95498f9884315f017a1
Luiz Henrique Ferreira <luizhff@gmail.com>
2023-01-20 21:09:54 -0300

    feat: add configs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,13 @@
 import { initializeApp } from 'firebase/app';
+import { firestore } from '../../config.json';
 
 const firebaseConfig = {
-  
+  apiKey: firestore.api_key,
+  authDomain: firestore.auth_domain,
+  projectId: firestore.project_id,
+  storageBucket: firestore.storage_bucket,
+  messagingSenderId: firestore.messaging_sender_id,
+  appId: firestore.app_id
 };
 
 const app = initializeApp(firebaseConfig);
--- src/services/login.js
+++ src/services/login.js
@@ -1,24 +1,29 @@
 import * as AuthSession from "expo-auth-session";
+import config from "../../config.json";
 
 class LoginWithGoogle {
   constructor(AuthSession) {
-    this.clientId = '';
-    this.redirectUri = '';
+    this.clientId = config.google.client_Id;
+    this.redirectUri = config.google.redirect_uri;
     this.AuthSession = AuthSession;
   }
 
   async signIn() {
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&response_type=token&scope=openid%20email%20profile`;
+    try {
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&response_type=token&scope=openid%20email%20profile`;
 
-    const { type, params } = await this.AuthSession.startAsync({ authUrl });
+      const { type, params } = await this.AuthSession.startAsync({ authUrl });
 
-    if (type === 'success') {
-      const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+      if (type === 'success') {
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
 
-      return await response.json();
+        return await response.json();
+      }
+
+      return null;
+    } catch (e) {
+      console.log(e);
     }
-
-    return null;
   }
 }
 

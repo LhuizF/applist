2367aa74a77840e693b391aa5f246a0f64a55e99
Luiz Henrique Ferreira <luizhff@gmail.com>
2023-01-20 22:03:46 -0300

    feat: add loading in login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import LoginWithGoogle from "../../services/login";
 import UserModel from "../../services/firebase/user";
 import LocalStorage from "../../storage";
@@ -6,28 +6,33 @@ import { Container } from '../../components/Templates/container';
 import { LoginContainer } from '../../components/Molecules/Login';
 
 export const Login = ({ navigation }) => {
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkUser = async () => {
       const user = await LocalStorage.getItem('user');
       if (user) {
+        console.log('Usuário logado')
         navigation.navigate('Home');
       }
+      setIsLoading(false);
     }
     checkUser();
   }, [])
 
   const signInWithGoogle = async () => {
+    setIsLoading(true);
     const userWithToken = await LoginWithGoogle.signIn();
     const data = await UserModel.loginUser(userWithToken);
     if (data.status === 'success') {
       await LocalStorage.setItem('user', data.user);
       navigation.navigate('Home');
     }
+    setIsLoading(false);
   }
 
   return (
-    <Container>
+    <Container isLoading={isLoading} >
       <LoginContainer onPress={signInWithGoogle} />
     </Container>
   );
